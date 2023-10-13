bd5acec99081fa3371a370d8e5cbb129ad69339e
Doan Ngoc <ngodoanngoc60@gmail.com>
2023-10-13 20:44:58 +0700

    Complete assignment 2.2.5

--- ShowTwoNumbers.java
+++ ShowTwoNumbers.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class ShowTwoNumbers {
     public static void main(String[] args){
         String strNum1, strNum2;
-        String strNotification = "You've just entered";
+        String strNotification = "You've just entered ";
 
         strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input the first number", JOptionPane.INFORMATION_MESSAGE);
         strNotification += strNum1 + " and ";
